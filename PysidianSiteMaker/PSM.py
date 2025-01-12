f8b17429c2fa55b723c75ea85c79927b64b39808
ArmoredThirteen <6456070+ArmoredThirteen@users.noreply.github.com>
2025-01-11 16:28:09 -0800

    Modified code style, updated files to use tabs, keep tabs on empty lines, slight mods to style

--- PysidianSiteMaker/PSM.py
+++ PysidianSiteMaker/PSM.py
@@ -10,28 +10,28 @@ vaultDir = sys.argv[1]
 
 
 def main():
-    sourceDir = vaultDir
-    targetDir = vaultDir + "_build"
-
-    # Clean build directory
-    if os.path.isdir(targetDir):
-        shutil.rmtree(targetDir)
-    os.makedirs(targetDir)
-
-    # Build single test page from the targetDir argument
-    headerFile = sourceDir + "/header.html"
-    bodySourceFile = sourceDir + "/index.md"
-    footerFile = sourceDir + "/footer.html"
-    target = targetDir + "/index.html"
-    PageBuilder.BuildPage(headerFile, bodySourceFile, footerFile, target)
-
-    # Copy over manually built files
-    toCopy = ["/style.css"]
-    for i in range(len(toCopy)):
-        sourceFile = sourceDir + toCopy[i]
-        targetDir = targetDir + toCopy[i]
-        shutil.copy2(sourceFile, targetDir)
+	sourceDir = vaultDir
+	targetDir = vaultDir + "_build"
+	
+	# Clean build directory
+	if os.path.isdir(targetDir):
+		shutil.rmtree(targetDir)
+	os.makedirs(targetDir)
+	
+	# Build single test page from the targetDir argument
+	headerFile = sourceDir + "/header.html"
+	bodySourceFile = sourceDir + "/index.md"
+	footerFile = sourceDir + "/footer.html"
+	target = targetDir + "/index.html"
+	PageBuilder.BuildPage(headerFile, bodySourceFile, footerFile, target)
+	
+	# Copy over manually built files
+	toCopy = ["/style.css"]
+	for i in range(len(toCopy)):
+		sourceFile = sourceDir + toCopy[i]
+		targetDir = targetDir + toCopy[i]
+		shutil.copy2(sourceFile, targetDir)
 
 
 if __name__ == '__main__':
-    main()
+	main()
--- PysidianSiteMaker/PageBuilder.py
+++ PysidianSiteMaker/PageBuilder.py
@@ -2,12 +2,12 @@ import markdown
 
 
 def BuildPage(headerFile, bodySourceFile, footerFile, target):
-    header = open(headerFile).read()
-    bodySource = open(bodySourceFile).read()
-    body = markdown.markdown(bodySource)
-    footer = open(footerFile).read()
-
-    with open(target, 'w') as f:
-        f.write(header)
-        f.write(body)
-        f.write(footer)
+	header = open(headerFile).read()
+	bodySource = open(bodySourceFile).read()
+	body = markdown.markdown(bodySource)
+	footer = open(footerFile).read()
+	
+	with open(target, 'w') as f:
+		f.write(header)
+		f.write(body)
+		f.write(footer)
